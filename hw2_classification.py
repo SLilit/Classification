93f616626e37a02529e498bbe9be5f111e9eb354
LILIT <lilit555ai@gmail.com>
2018-09-10 15:23:17 -0600

    Read data

--- hw2_classification.py
+++ hw2_classification.py
@@ -4,4 +4,13 @@ import pandas as pd
 import sys
 import math
 
+data = pd.read_excel('C:/Users/DENVER/edX/ML/d.xlsx', sheetname = 0)
+data1 = pd.read_excel('C:/Users/DENVER/edX/ML/t.xlsx', sheetname = 0)
+X_train = np.array(data.iloc[:, :5])
+y_train = np.array(data.iloc[:, -1:])
+X_test = np.array(data1.iloc[:, :])
+#print('train: ', X_train[:4])
+#print('test: ', y_train[:4])
+#print('test: ', X_test[:4])
 
+d
